4ff63df3f31fad10122178e6eb48c628574d6037
Hussein Fawaz <husseinfawaz@MacBook-Pro-7.local>
2024-08-01 19:43:53 -0700

    Completed the following: 1. Styled all sections of the website 2. Integrated two fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -117,99 +117,117 @@ export default function HomePage () {
   }, []);
 
   return (
-    <div>
-      <input
-        className="w-full"
-        type="text"
-        placeholder="Search movie by title (example: &quot;Spider-Man&quot;)"
-        onChange={(event) => {
-          if (isFetchError) setIsFetchError(false);
-          setTitle(event.target.value);
-        }}
-        value={title}
-      />
+    <main className="w-full max-w-screen-lg flex flex-col gap-16 m-auto px-4 mb-16 text-lg">
 
-      <button
-        className="w-full"
-        type="button"
-        onClick={reset}
-      >
-        Clear
-      </button>
+      <div className="w-[200px] h-[200px] bg-white/40 blur-[128px] rounded-full fixed -top-[100px] -left-[100px] -z-10"></div>
+      <div className="w-[200px] h-[200px] bg-white/40 blur-[128px] rounded-full fixed -top-[100px] -right-[100px] -z-10"></div>
+
+      <h1 className="font-limelight-regular uppercase text-3xl text-center mt-16">Ensemble<br/>Cinemas</h1>
+
+      <div className="flex flex-col gap-8">
+        <h2 className="font-roboto-bold w-full text-2xl">Search Movies</h2>
+        <div className="flex gap-4">
+          <input
+            className="w-full rounded-full px-4 py-2 bg-transparent border-2 border-gray-700/60 focus:border-gray-500 focus:outline-none placeholder-gray-700 focus:placeholder-gray-500 focus:ring-0"
+            type="text"
+            placeholder="Search movie by title (example: &quot;Spider-Man&quot;)"
+            onChange={(event) => {
+              if (isFetchError) setIsFetchError(false);
+              setTitle(event.target.value);
+            }}
+            value={title}
+          />
+          <button
+            className="p-4 font-roboto-bold bg-gray-700/60 rounded-full disabled:opacity-40 hover:bg-gray-700 disabled:pointer-events-none"
+            type="button"
+            onClick={reset}
+            disabled={title.length === 0}
+          >
+            <svg width="24px" height="24px" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#fff"><path d="M6.75827 17.2426L12.0009 12M17.2435 6.75736L12.0009 12M12.0009 12L6.75827 6.75736M12.0009 12L17.2435 17.2426" stroke="#fff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path></svg>
+          </button>
+        </div>
+      </div>
 
       {isFetchError && (
-        <div>Sorry! We could not find any movies with the title &quot;{title}&quot;</div>
-      )}
-
-      {!isFetchError && movies.length === 0 && recents.length > 0 && (
-        <div>
-          <p>Recent Searches</p>
-          <button
-            className="w-full"
-            type="button"
-            onClick={() => clearSearchTerms()}
-          >
-            Clear Recent Searches
-          </button>
-          <div>
-            {recents.map((searchTerm: string, index: number) => (
-              <div key={index}>
-                <button
-                  className="w-full"
-                  type="button"
-                  onClick={() => setTitle(searchTerm)}
-                >
-                  {searchTerm}
-                </button>
-              </div>
-            ))}
-          </div>
-        </div>
+        <p className="text-center">Sorry! We could not find any movies with the title &quot;{title}&quot;</p>
       )}
 
       {!isFetchError && movies.length === 0 && (
-        <div>
-          <p>Suggested Searches</p>
-          <div>
-            {suggestions.map((suggestion: string, index: number) => (
-              <div key={index}>
-                <button
-                  className="w-full"
-                  type="button"
-                  onClick={() => setTitle(suggestion)}
-                >
-                  {suggestion}
-                </button>
-              </div>
-            ))}
+        <div className="relative flex flex-col gap-16">
+          <div className="flex flex-col gap-8">
+            <h2 className="font-roboto-bold w-full text-2xl">Suggestions</h2>
+            <div className="flex flex-wrap gap-4">
+              {suggestions.map((suggestion: string, index: number) => (
+                <div key={index}>
+                  <button
+                    className="px-4 py-2 rounded-full hover:bg-gray-700 hover:border-gray-700 bg-gray-700/60 border-2 border-gray-700/60"
+                    type="button"
+                    onClick={() => setTitle(suggestion)}
+                  >
+                    {suggestion}
+                  </button>
+                </div>
+              ))}
+            </div>
           </div>
+
+          <div className="w-[200px] h-[200px] bg-white/40 blur-[128px] rounded-full absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 -z-10"></div>
+
+          {recents.length > 0 && (
+            <div className="flex flex-col gap-8">
+              <h2 className="font-roboto-bold w-full text-2xl">Recents</h2>
+              <div className="flex flex-wrap gap-4">
+                {recents.map((searchTerm: string, index: number) => (
+                  <div key={index}>
+                    <button
+                      className="px-4 py-2 rounded-full hover:bg-gray-700 hover:border-gray-700 bg-gray-700/60 border-2 border-gray-700/60"
+                      type="button"
+                      onClick={() => setTitle(searchTerm)}
+                    >
+                      {searchTerm}
+                    </button>
+                  </div>
+                ))}
+              </div>
+              <button
+                className="px-4 py-2 rounded-full hover:bg-sky-500 border-2 border-sky-500/60 self-start"
+                type="button"
+                onClick={() => clearSearchTerms()}
+              >
+                Clear All
+              </button>
+            </div>
+          )}
         </div>
       )}
 
       {!isFetchError && movies.length > 0 && (
-        <div>
-          {movies.map((movie: T_Movie, index: number) => (
-            <div key={index}>
-              {movie.poster !== 'N/A' && (
-                <img src={movie.poster} alt={movie.title} width="200px" height="300px" />
-              )}
-              {movie.poster === 'N/A' && (
-                <div className="w-[200px] h-[300px] bg-gray-800"></div>
-              )}
-              <p>{movie.title}</p>
-              <p>{movie.yearOfRelease}</p>
-              <button
-                className="w-full"
-                type="button"
-                onClick={() => {}}
-              >
-                Click Me
-              </button>
-            </div>
-          ))}
-
+        <div className="flex flex-col gap-8">
+          <h2 className="font-roboto-bold w-full text-2xl">Results</h2>
+          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-x-4 gap-y-8">
+            {movies.map((movie: T_Movie, index: number) => (
+              <div key={index} className="flex flex-col gap-4">
+                <div className="aspect-[2/3]">
+                  {movie.poster !== 'N/A' && (
+                    <img src={movie.poster} alt={movie.title} width="200px" height="300px" className="rounded-lg w-full" />
+                  )}
+                  {movie.poster === 'N/A' && (
+                    <img src={`https://placehold.co/200x300/374151/white/?text=${movie.title}&font=roboto/`} className="rounded-lg w-full" />
+                  )}
+                </div>
+                <p>{movie.title} ({movie.yearOfRelease})</p>
+                <button
+                  className="px-4 py-2 rounded-full hover:bg-gray-700 hover:border-gray-700 bg-gray-700/60 border-2 border-gray-700/60 mt-auto"
+                  type="button"
+                  onClick={() => {}}
+                >
+                  Button
+                </button>
+              </div>
+            ))}
+          </div>
           <button
-            className="w-full"
+            className="px-4 py-2 rounded-full hover:bg-sky-500 border-2 border-sky-500/60 self-center disabled:opacity-40 disabled:pointer-events-none"
             type="button"
             onClick={fetchMoviesNextPage}
             disabled={isDisableButton || page === 100}
@@ -218,10 +236,16 @@ export default function HomePage () {
           </button>
 
           {isDisableButton && (
-            <p>Wow! You have viewed all movies with the title &quot;{title}&quot;</p>
+            <p className="text-center">Wow! You have viewed all movies with the title &quot;{title}&quot;</p>
           )}
         </div>
       )}
-    </div>
+
+      <div className="h-[2px] w-full bg-gray-700/60 rounded-full"></div>
+
+      <footer>
+        <p className="text-center text-gray-700/60">Designed and built by Hussein Fawaz</p>
+      </footer>
+    </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,11 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    fontFamily: {
+      'limelight-regular': 'Limelight Regular',
+      'roboto-regular': 'Roboto Regular',
+      'roboto-bold': 'Roboto Bold',
+    }
   },
   plugins: [],
 };
